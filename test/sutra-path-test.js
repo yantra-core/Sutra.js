2482acd491e166a017721a222d41b97b26295e60
Marak <marak.squires@gmail.com>
2023-11-28 16:24:10 -0500

    Can now find and update nodes in a Sutra tree Adds `Sutra.findPath` Adds `Sutra.updateNode` Adds tests for find nodes / updating nodes

---
+++ test/sutra-path-test.js
@@ -0,0 +1,182 @@
+import tap from 'tape';
+import Sutra from '../lib/Sutra.js';
+
+tap.test('Sutra Library Tests', (parent) => {
+
+  parent.test('should handle incorrect data types for "if" and "then" properties', (t) => {
+    const sutra = new Sutra();
+  
+    // Test case with "if" as a string
+    const nodeWithIfString = {
+      if: 'incorrectString',
+      then: [{ action: 'testAction' }]
+    };
+    sutra.addAction(nodeWithIfString);
+    const ifStringUpdate = sutra.updateNode('tree.if', { if: 'isFalse' });
+    t.equal(ifStringUpdate, false, 'updateNode should return false or not update when "if" is a string');
+  
+    // Test case with "then" as a string
+    const nodeWithThenString = {
+      if: 'isTrue',
+      then: 'incorrectString'
+    };
+    sutra.addAction(nodeWithThenString);
+    const thenStringUpdate = sutra.updateNode('tree.if', { if: 'isFalse' });
+    t.equal(thenStringUpdate, false, 'updateNode should return false or not update when "then" is a string');
+  
+    t.end();
+  });
+  
+  parent.test('should add and find a node using sutraPath', (t) => {
+    const sutra = new Sutra();
+    const testNode = { if: 'isTrue', then: [{ action: 'testAction' }] };
+    sutra.addAction(testNode);
+
+    // Assuming the first node added gets sutraPath 'this.tree[0]'
+    const foundNode = sutra.findNode('tree[0]');
+    console.log('foundNodefoundNodefoundNode', foundNode)
+    t.equal(foundNode, testNode, 'findNode should retrieve the correct node using sutraPath');
+    t.end();
+  });
+
+  parent.test('should update a node correctly using sutraPath', (t) => {
+
+    const sutra = new Sutra();
+    // ... add conditions ...
+
+    sutra.addAction({ if: 'isTrue', then: [{ action: 'testAction' }] });
+
+    const updatedNodeData = { if: 'isFalse', then: [{ action: 'anotherAction' }] };
+    const updateSuccess = sutra.updateNode('tree[0]', updatedNodeData);
+
+    t.equal(updateSuccess, true, 'updateNode should return true on success');
+    t.equal(sutra.findNode('tree[0]').if, 'isFalse', 'updateNode should update the condition using sutraPath');
+    t.end();
+  });
+
+  parent.test('should update a deeply nested node correctly using sutraPath', (t) => {
+    const sutra = new Sutra();
+
+    // Adding conditions for testing
+    sutra.addCondition('isTrue', () => true);
+    sutra.addCondition('isFalse', () => false);
+
+    // Create a nested node structure
+    const nestedNode = {
+      if: 'isTrue',
+      then: [
+        {
+          if: 'isTrue',
+          then: [{ action: 'nestedAction' }],
+          else: [{ action: 'elseAction' }]
+        }
+      ]
+    };
+    sutra.addAction(nestedNode);
+
+    // Update the deeply nested node using its sutraPath
+    const updatedNestedNodeData = { if: 'isFalse' };
+    const updateNestedSuccess = sutra.updateNode('tree[0].then[0]', updatedNestedNodeData);
+
+    t.equal(updateNestedSuccess, true, 'updateNode should return true on success for deeply nested node');
+    t.equal(sutra.findNode('tree[0].then[0]').if, 'isFalse', 'updateNode should update the condition of the deeply nested node using sutraPath');
+    t.end();
+  });
+
+  parent.test('should handle invalid sutraPath correctly', (t) => {
+    const sutra = new Sutra();
+    sutra.addAction({ if: 'isTrue', then: [{ action: 'testAction' }] });
+  
+    const invalidPath = 'tree[1]';
+    const foundNode = sutra.findNode(invalidPath);
+    t.equal(foundNode, undefined, 'findNode should return undefined for invalid sutraPath');
+    t.end();
+  });
+  
+  parent.test('should handle missing array index correctly', (t) => {
+    const sutra = new Sutra();
+    sutra.addAction({ if: 'isTrue', then: [{ action: 'testAction' }] });
+  
+    const missingIndex = 'tree[0].then[1]';
+    const foundNode = sutra.findNode(missingIndex);
+    t.equal(foundNode, undefined, 'findNode should return undefined for missing array index');
+    t.end();
+  });
+  
+  parent.test('should handle empty tree correctly', (t) => {
+    const sutra = new Sutra();
+  
+    const emptyPath = 'tree[0]';
+    const foundNode = sutra.findNode(emptyPath);
+    t.equal(foundNode, undefined, 'findNode should return undefined when tree is empty');
+    t.end();
+  });
+
+  parent.test('should handle updating non-existent node correctly', (t) => {
+    const sutra = new Sutra();
+    // ... Create some structure ...
+  
+    const nonExistentPath = 'tree[1]';
+    const updateSuccess = sutra.updateNode(nonExistentPath, { if: 'isFalse' });
+    t.equal(updateSuccess, false, 'updateNode should return false when trying to update a non-existent node');
+    t.end();
+  });
+  
+  parent.test('should handle finding non-existent node correctly', (t) => {
+    const sutra = new Sutra();
+    // ... Create some structure ...
+  
+    const nonExistentPath = 'tree[1]';
+    const foundNode = sutra.findNode(nonExistentPath);
+    t.equal(foundNode, undefined, 'findNode should return undefined when trying to find a non-existent node');
+    t.end();
+  });
+
+  parent.test('should update a complex nested node correctly using sutraPath', (t) => {
+    const sutra = new Sutra();
+  
+    // Adding conditions for testing
+    sutra.addCondition('isTrue', () => true);
+    sutra.addCondition('isFalse', () => false);
+  
+    // Create a complex nested node structure
+    const complexNestedNode = {
+      if: 'isTrue',
+      then: [
+        {
+          if: 'isTrue',
+          then: [
+            {
+              if: 'isTrue',
+              then: [{ action: 'deepAction1' }],
+              else: [{ action: 'deepAction2' }]
+            },
+            {
+              if: 'isTrue',
+              then: [{ action: 'deepAction3' }],
+              else: [{ action: 'deepAction4' }]
+            }
+          ],
+          else: [{ action: 'elseAction1' }]
+        },
+        {
+          if: 'isTrue',
+          then: [{ action: 'simpleAction' }]
+        }
+      ],
+      else: [{ action: 'elseAction2' }]
+    };
+    sutra.addAction(complexNestedNode);
+  
+    // Update a deeply nested node using its sutraPath
+    const updatedNestedNodeData = { if: 'isFalse', then: [{ action: 'updatedAction' }] };
+    const updateNestedSuccess = sutra.updateNode('tree[0].then[0].then[1]', updatedNestedNodeData);
+  
+    t.equal(updateNestedSuccess, true, 'updateNode should return true on success for deeply nested node');
+    const updatedNode = sutra.findNode('tree[0].then[0].then[1]');
+    t.equal(updatedNode.if, 'isFalse', 'updateNode should update the condition of the deeply nested node');
+    t.equal(updatedNode.then[0].action, 'updatedAction', 'updateNode should update the action of the deeply nested node');
+    t.end();
+  });
+
+});
---
+++ test/sutra-update-condition-test.js
@@ -0,0 +1,146 @@
+import tap from 'tape';
+import Sutra from '../lib/Sutra.js';
+
+tap.test('Sutra Library Tests', async (parent) => {
+  // Test for condition evaluation
+  parent.test('should correctly evaluate conditions', async (t) => {
+    const sutra = new Sutra();
+    sutra.addCondition('isTrue', () => true);
+    t.equal(sutra.evaluateCondition('isTrue', {}), true, 'isTrue condition should return true');
+  });
+
+  // Test for action execution based on conditions
+  parent.test('should correctly handle actions based on conditions', async (t) => {
+    const sutra = new Sutra();
+    let actionExecuted = false;
+
+    sutra.on('testAction', (data) => {
+      actionExecuted = true;
+    });
+
+    sutra.addCondition('isTrue', () => true);
+    sutra.addAction({
+      if: 'isTrue',
+      then: [{ action: 'testAction' }]
+    });
+
+    sutra.traverseNode(sutra.tree[0], {});
+    t.equal(actionExecuted, true, 'testAction should be executed when isTrue condition is met');
+  });
+
+  // Test for event emission
+  parent.test('should emit events correctly', async (t) => {
+    const sutra = new Sutra();
+    let eventEmitted = false;
+
+    sutra.on('customEvent', () => {
+      eventEmitted = true;
+    });
+
+    sutra.emit('customEvent');
+    t.equal(eventEmitted, true, 'customEvent should be emitted correctly');
+  });
+
+  // Test for complex nested conditions and actions
+  parent.test('should handle nested conditions and actions', async (t) => {
+    const sutra = new Sutra();
+    let nestedActionExecuted = false;
+
+    sutra.addCondition('isTrue', () => true);
+    sutra.addCondition('isAlsoTrue', () => true);
+    sutra.addCondition('isAlsoFalse', () => false);
+
+    // Test when both conditions are true
+    sutra.addAction({
+      if: 'isTrue',
+      then: [
+        {
+          if: 'isAlsoTrue',
+          then: [{ action: 'nestedAction' }]
+        }
+      ]
+    });
+
+    sutra.on('nestedAction', () => {
+      nestedActionExecuted = true;
+    });
+
+    sutra.traverseNode(sutra.tree[0], {});
+    t.equal(nestedActionExecuted, true, 'nestedAction should be executed when both conditions are met');
+
+    // Reset and test when the second condition is false
+    nestedActionExecuted = false;
+    sutra.tree = []; // Reset the behavior tree
+    sutra.addAction({
+      if: 'isTrue',
+      then: [
+        {
+          if: 'isAlsoFalse',
+          then: [{ action: 'nestedAction' }]
+        }
+      ]
+    });
+
+    sutra.traverseNode(sutra.tree[0], {});
+    t.equal(nestedActionExecuted, false, 'nestedAction should not be executed when the second condition is false');
+  });
+
+  parent.test('Sutra DSL Conditions', (t) => {
+    const sutra = new Sutra();
+
+    // Test lessThan condition
+    sutra.addCondition('isHealthLow', {
+      op: 'lessThan',
+      property: 'health',
+      value: 50
+    });
+
+    // Test data
+    const testData = { health: 40 };
+
+    t.equal(sutra.evaluateCondition('isHealthLow', testData), true, 'isHealthLow should return true for health < 50');
+
+    t.end();
+  });
+
+
+  // Test for DSL-based condition evaluation
+  parent.test('should correctly evaluate DSL conditions', async (t) => {
+    const sutra = new Sutra();
+
+    // Adding a condition using the new DSL syntax
+    sutra.addCondition('isHealthLow', {
+      op: 'lessThan',
+      property: 'health',
+      value: 50
+    });
+
+    t.equal(sutra.evaluateCondition('isHealthLow', { health: 40 }), true, 'isHealthLow should return true for health < 50');
+    t.equal(sutra.evaluateCondition('isHealthLow', { health: 60 }), false, 'isHealthLow should return false for health >= 50');
+
+    t.end();
+  });
+
+  tap.test('Sutra DSL op Tests', (t) => {
+    const sutra = new Sutra();
+
+    // Test each DSL op
+    sutra.addCondition('lessThanTest', { op: 'lessThan', property: 'value', value: 10 });
+    sutra.addCondition('greaterThanTest', { op: 'greaterThan', property: 'value', value: 10 });
+    sutra.addCondition('equalsTest', { op: 'equals', property: 'value', value: 10 });
+    sutra.addCondition('notEqualsTest', { op: 'notEquals', property: 'value', value: 10 });
+    sutra.addCondition('lessThanOrEqualTest', { op: 'lessThanOrEqual', property: 'value', value: 10 });
+    sutra.addCondition('greaterThanOrEqualTest', { op: 'greaterThanOrEqual', property: 'value', value: 10 });
+
+    // Perform tests
+    t.equal(sutra.evaluateCondition('lessThanTest', { value: 5 }), true, 'lessThanTest should return true for value < 10');
+    t.equal(sutra.evaluateCondition('greaterThanTest', { value: 15 }), true, 'greaterThanTest should return true for value > 10');
+    t.equal(sutra.evaluateCondition('equalsTest', { value: 10 }), true, 'equalsTest should return true for value == 10');
+    t.equal(sutra.evaluateCondition('notEqualsTest', { value: 15 }), true, 'notEqualsTest should return true for value != 10');
+    t.equal(sutra.evaluateCondition('lessThanOrEqualTest', { value: 10 }), true, 'lessThanOrEqualTest should return true for value <= 10');
+    t.equal(sutra.evaluateCondition('greaterThanOrEqualTest', { value: 10 }), true, 'greaterThanOrEqualTest should return true for value >= 10');
+
+    t.end();
+  });
+
+});
