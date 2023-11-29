452945dd63d25659116fa34d3ce26698790a77b6
Marak <marak.squires@gmail.com>
2023-11-29 17:06:38 -0500

    Sutra conditions can now be arrays, default join logical AND

--- test/sutra-array-conditions-test.js
+++ test/sutra-array-conditions-test.js
@@ -1,65 +1,102 @@
 import tap from 'tape';
 import Sutra from '../lib/sutra.js';
 
-tap.test('Array of Conditions Test - Sequential Execution', async (t) => {
-  let sutra = new Sutra();
-  let actionExecuted = false;
+tap.test('Sutra - Array of Conditions Tests', async (parent) => {
 
-  // Adding conditions as an array
-  sutra.addCondition('arrayConditions', [
-    (entity) => entity.type === 'BOSS', // First condition as a function
-    {
-      op: 'lessThan', // Second condition as an object
-      property: 'health',
-      value: 50
-    }
-  ]);
+  parent.test('Sequential Execution - All Conditions Met', async (t) => {
+    let sutra = new Sutra();
+    let actionExecuted = false;
 
-  sutra.addAction({
-    if: 'arrayConditions',
-    then: [{ action: 'testAction' }]
+    sutra.addCondition('sequentialConditions', [
+      (entity) => entity.type === 'BOSS',
+      { op: 'lessThan', property: 'health', value: 50 }
+    ]);
+
+    sutra.addAction({ if: 'sequentialConditions', then: [{ action: 'testAction' }] });
+    sutra.on('testAction', () => { actionExecuted = true; });
+
+    let entity = { id: 1, type: 'BOSS', health: 30 };
+    sutra.tick(entity);
+    t.equal(actionExecuted, true, 'Action should be executed when all conditions are met');
+    t.end();
   });
 
-  sutra.on('testAction', () => {
-    actionExecuted = true;
+  parent.test('Sequential Execution - One Condition Not Met', async (t) => {
+    let sutra = new Sutra();
+    let actionExecuted = false;
+
+    sutra.addCondition('sequentialConditions', [
+      (entity) => entity.type === 'BOSS',
+      { op: 'greaterThan', property: 'health', value: 50 }
+    ]);
+
+    sutra.addAction({ if: 'sequentialConditions', then: [{ action: 'testAction' }] });
+    sutra.on('testAction', () => { actionExecuted = true; });
+
+    let entity = { id: 2, type: 'BOSS', health: 30 };
+    sutra.tick(entity);
+    t.equal(actionExecuted, false, 'Action should not be executed when one condition is not met');
+    t.end();
   });
 
-  // Entity that meets both conditions in the array
-  let entity = { id: 1, type: 'BOSS', health: 30 };
-
-  sutra.tick(entity);
-  t.equal(actionExecuted, true, 'Action should be executed when all array conditions are met');
-
-  t.end();
-});
-
-tap.only('Array of Conditions Test - Partial Match', async (t) => {
-  let sutra = new Sutra();
-  let actionExecuted = false;
-
-  // Similar setup as the previous test, but with different conditions
-  sutra.addCondition('arrayConditionsPartial', [
-    (entity) => entity.type === 'BOSS', // Only the first condition should match
-    {
-      op: 'greaterThan',
-      property: 'health',
-      value: 50
-    }
-  ]);
-
-  sutra.addAction({
-    if: 'arrayConditionsPartial',
-    then: [{ action: 'testAction' }]
+  parent.test('Spawner Action with Composite Conditions', async (t) => {
+    let sutra = new Sutra();
+    let blockCreated = false;
+  
+    sutra.addCondition('blockCountLessThan5', { op: 'lessThan', gamePropertyPath: 'ents.BLOCK.length', value: 5 });
+    sutra.addCondition('spawnerHealthAbove50', { op: 'greaterThan', property: 'health', value: 50 });
+  
+    sutra.addAction({
+      if: ['blockCountLessThan5', 'spawnerHealthAbove50'],
+      then: [{ action: 'createBlock' }]
+    });
+  
+    sutra.on('createBlock', () => { blockCreated = true; });
+  
+    const gameState = { ents: { BLOCK: [] } };
+    const spawner = { type: 'SPAWNER', health: 60 };
+  
+    sutra.tick(spawner, gameState);
+    t.equal(blockCreated, true, 'Block should be created when both conditions are met');
+  
+    blockCreated = false;
+    spawner.health = 40; // Health below 50
+    sutra.tick(spawner, gameState);
+    t.equal(blockCreated, false, 'Block should not be created when one condition is not met');
+    t.end();
   });
 
-  sutra.on('testAction', () => {
-    actionExecuted = true;
+  parent.test('Spawner Action with Nested Composite Conditions', async (t) => {
+    let sutra = new Sutra();
+    let blockCreated = false;
+  
+    sutra.addCondition('isSpawner', (entity) => entity.type === 'SPAWNER');
+  
+    sutra.addCondition('blockCountLessThan5', { op: 'lessThan', gamePropertyPath: 'ents.BLOCK.length', value: 5 });
+    sutra.addCondition('spawnerHealthAbove50', { op: 'greaterThan', property: 'health', value: 50 });
+  
+    sutra.addAction({
+      if: 'isSpawner',
+      then: [{
+        if: ['blockCountLessThan5', 'spawnerHealthAbove50'],
+        then: [{ action: 'createBlock' }]
+      }]
+    });
+  
+    sutra.on('createBlock', () => { blockCreated = true; });
+  
+    const gameState = { ents: { BLOCK: [] } };
+    const spawner = { type: 'SPAWNER', health: 60 };
+  
+    sutra.tick(spawner, gameState);
+    t.equal(blockCreated, true, 'Block should be created when both conditions are met');
+  
+    blockCreated = false;
+    spawner.health = 40; // Health below 50
+    sutra.tick(spawner, gameState);
+    t.equal(blockCreated, false, 'Block should not be created when one condition is not met');
+    t.end();
   });
-
-  // Entity that meets both conditions in the array
-  let entity = { id: 2, type: 'BOSS', health: 100 };
-
-  sutra.tick(entity);
-  t.equal(actionExecuted, true, 'Action should be executed when all array conditions are met');
+  
 
 });
