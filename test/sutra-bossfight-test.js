440fc32b993c918e42b2563aa68b1cf823f9f352
Marak <marak.squires@gmail.com>
2023-11-30 00:26:47 -0500

    Ensures gameState is continued for composition condition functions Adds integration tests for conditions as functions with gameState

--- test/sutra-bossfight-test.js
+++ test/sutra-bossfight-test.js
@@ -9,8 +9,16 @@ tap.test('Boss fight behavior tree test', async (t) => {
 
   let sutra = new Sutra();
 
+  // Simulated gameState
+  let gameState = {
+    ents: { BLOCK: [] } // Start with empty BLOCK array
+  };
+
   // use custom function for condition
   sutra.addCondition('isBoss', (entity) => entity.type === 'BOSS');
+
+  // use custom function for condition that checks gameState property
+  sutra.addCondition('blockCountEquals0', (data, gameState) => gameState.ents.BLOCK.length === 0);
 
   /*
   // could also be written as:
@@ -20,8 +28,7 @@ tap.test('Boss fight behavior tree test', async (t) => {
     value: 'BOSS'
   });
   */
- 
-  
+
   // use standard Sutra DSL for condition
   sutra.addCondition('isHealthLow', {
     op: 'lessThan',
@@ -31,6 +38,7 @@ tap.test('Boss fight behavior tree test', async (t) => {
 
   let bossHealthLowDetected = false;
   let updatedEnt = {};
+  let blockCountEquals0Detected = false;
 
   sutra.on('entity::update', (updatedEntity, node) => {
     //console.log('entity::update', updatedEntity, node);
@@ -42,6 +50,10 @@ tap.test('Boss fight behavior tree test', async (t) => {
     }
   });
 
+  sutra.on('blockCountEquals0Detected', (updatedEntity, node) => {
+    blockCountEquals0Detected = true;
+  });
+
   // Function to generate a random color integer
   function generateRandomColorInt() {
     return Math.floor(Math.random() * 255);
@@ -51,41 +63,58 @@ tap.test('Boss fight behavior tree test', async (t) => {
     if: 'isBoss',
     then: [{
       if: 'isHealthLow',
-      then: [{ 
-        action: 'entity::update', 
+      then: [{
+        action: 'entity::update',
         data: { color: generateRandomColorInt, speed: 5 } // Example with multiple properties
       }]
     }]
   });
-  
+
   function gameTick() {
     allEntities.forEach(entity => {
-      sutra.tick(entity);
+      sutra.tick(entity, gameState);
     });
+    // Check block count condition
+    blockCountEquals0Detected = sutra.evaluateCondition('blockCountEquals0', {}, gameState);
+
   }
 
   // Test case: Boss health is above 50, should not detect
   gameTick();
   t.equal(bossHealthLowDetected, false, 'Boss with health above 50 should not be detected');
+  t.equal(blockCountEquals0Detected, true, 'BLOCK count equals 0 should be detected');
+
 
   // Test case: Boss health drops below 50, should detect
   allEntities[0].health = 30;
   gameTick();
   t.equal(bossHealthLowDetected, true, 'Boss with health below 50 should be detected');
+  t.equal(blockCountEquals0Detected, true, 'BLOCK count equals 0 should still be detected');
 
   // check random value from 0-255
   t.ok(updatedEnt.color >= 0 && updatedEnt.color <= 255, 'Boss has random color');
-  
+
   // check that speed is 5
   t.equal(updatedEnt.speed, 5, 'Boss has speed of 5');
+
+
+  // Test case: Add a BLOCK, check BLOCK count not equals 0
+  gameState.ents.BLOCK.push({});
+  gameTick();
+  t.equal(blockCountEquals0Detected, false, 'BLOCK count not equals 0 should be detected');
+
+
 
   // Resetting for next test
   bossHealthLowDetected = false;
   allEntities[0].health = 100;
+  gameState.ents.BLOCK = [];
+
 
   // Test case: Boss health goes back above 50, should not detect again
   gameTick();
   t.equal(bossHealthLowDetected, false, 'Boss with restored health above 50 should not be detected');
+  t.equal(blockCountEquals0Detected, true, 'BLOCK count reset to 0 should be detected');
 
   t.end();
 });
