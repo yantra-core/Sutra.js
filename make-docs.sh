507a5106510f4b8cbae5eea26404f20f4b589f54
Marak <marak.squires@gmail.com>
2024-02-04 17:01:48 -0500

    Adds docs scripts

---
+++ make-docs.sh
@@ -0,0 +1,2 @@
+# npx typedoc --out docs/ types.ts --readme none
+npx typedoc --out ./examples/browser/docs/ types.ts --basePath ./ --readme none --plugin typedoc-material-theme --themeColor '#007fff'
